de3beed88c36cc9ba6735d8bde4c87ccb76cf423
OvyWanKenobi <imovy1997@gmai.com>
2021-02-07 12:20:38 +0600

    Adding files

---
+++ Lab2-1/Lab2-1a/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Input the end of range:  ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Even Numbers :");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab2-1/Lab2-1b/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even Numbers from 1 to 100 :");
+            for (int i=1; i<=100; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.Write(i+" ");
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Odd Numbers from 1 to 100 :");
+            for (int i=1; i<=100; i++)
+            {
+                if (i%2 != 0)
+                {
+                    Console.Write(i+" ");
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
